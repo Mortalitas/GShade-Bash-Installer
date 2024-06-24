de25a472ba2501eb15256e48683f5c7406d45911
Marot <crazedragon@gmail.com>
2024-06-24 04:29:43 -0400

    Cleanup code

--- gshade_installer.sh
+++ gshade_installer.sh
@@ -1025,24 +1025,24 @@ presetAndShaderUpdate() {
   timestamp=$(date +"%Y-%m-%d/%T")
   if [ -f "version" ]; then performBackup "$timestamp"; [ -d "$GShadeHome/git" ] && gitUpdate "$timestamp"; fi
   printf "Updating presets..."
-  curl -sLO https://gitlab.com/Mortalitas/GShade-Presets/-/archive/master/GShade-Presets-master.zip
-  curl -sLO https://gitlab.com/Mortalitas/GShade-C-Shaders/-/archive/main/GShade-C-Shaders-main.zip
-  curl -sLo iMMERSE-main.zip https://github.com/martymcmodding/iMMERSE/archive/refs/heads/main.zip
-  curl -sLo METEOR-main.zip https://github.com/martymcmodding/METEOR/archive/refs/heads/main.zip
-  curl -sLo SHADERDECK-main.zip https://github.com/TreyM/SHADERDECK/archive/refs/heads/main.zip
+  curl -sLO "https://gitlab.com/Mortalitas/GShade-Presets/-/archive/master/GShade-Presets-master.zip"
+  curl -sLO "https://gitlab.com/Mortalitas/GShade-C-Shaders/-/archive/main/GShade-C-Shaders-main.zip"
+  curl -sLo "iMMERSE-main.zip" "https://github.com/martymcmodding/iMMERSE/archive/refs/heads/main.zip"
+  curl -sLo "METEOR-main.zip" "https://github.com/martymcmodding/METEOR/archive/refs/heads/main.zip"
+  curl -sLo "SHADERDECK-main.zip" "https://github.com/TreyM/SHADERDECK/archive/refs/heads/main.zip"
   mkdir tmpDir
   if [ "$IS_MAC" = true ] ; then
-    ditto -xk GGShade-Presets-master.zip tmpDir/gshade-presets && rm GShade-Presets-master.zip && cp -af tmpDir/gshade-presets/GShade-Presets-master/FFXIV/* gshade-presets #has to be extracted with ditto because of special chars on APFS
-    ditto -xk GShade-C-Shaders.zip tmpDir && rm GShade-C-Shaders.zip && cp -af tmpDir/GShade-C-Shaders-main/gshade-shaders .
-    ditto -xk iMMERSE-main.zip tmpDir && rm iMMERSE-main.zip && cp -af tmpDir/iMMERSE-main/Shaders gshade-shaders && cp -af tmpDir/iMMERSE-main/Textures gshade-shaders
-    ditto -xk METEOR-main.zip tmpDir && rm METEOR-main.zip && cp -af tmpDir/METEOR-main/Shaders gshade-shaders && cp -af tmpDir/METEOR-main/Textures gshade-shaders
-    ditto -xk SHADERDECK-main.zip tmpDir && rm SHADERDECK-main.zip && cp -af tmpDir/SHADERDECK-main/Shaders gshade-shaders && cp -af tmpDir/SHADERDECK-main/textures/* gshade-shaders/Textures && rm -r tmpDir #just case-sensitivity things
+    ditto -xk "GGShade-Presets-master.zip" "tmpDir/gshade-presets" && rm "GShade-Presets-master.zip" && cp -af "tmpDir/gshade-presets/GShade-Presets-master/FFXIV/*" "gshade-presets" #has to be extracted with ditto because of special chars on APFS
+    ditto -xk "GShade-C-Shaders.zip" "tmpDir" && rm "GShade-C-Shaders.zip" && cp -af "tmpDir/GShade-C-Shaders-main/gshade-shaders" "."
+    ditto -xk "iMMERSE-main.zip" "tmpDir" && rm "iMMERSE-main.zip" && cp -af "tmpDir/iMMERSE-main/Shaders" "gshade-shaders" && cp -af "tmpDir/iMMERSE-main/Textures" "gshade-shaders"
+    ditto -xk "METEOR-main.zip" "tmpDir" && rm "METEOR-main.zip" && cp -af "tmpDir/METEOR-main/Shaders" "gshade-shaders" && cp -af "tmpDir/METEOR-main/Textures" "gshade-shaders"
+    ditto -xk "SHADERDECK-main.zip" "tmpDir" && rm "SHADERDECK-main.zip" && cp -af "tmpDir/SHADERDECK-main/Shaders" "gshade-shaders" && cp -af "tmpDir/SHADERDECK-main/textures/*" "gshade-shaders/Textures" && rm -r "tmpDir" #just case-sensitivity things
   else
-    unzip -qquo GShade-Presets-master.zip -d tmpDir/gshade-presets && rm GShade-Presets-master.zip && cp -af tmpDir/gshade-presets/GShade-Presets-master/FFXIV/* gshade-presets
-    unzip -qquo GShade-C-Shaders-main.zip -d tmpDir && rm GShade-C-Shaders-main.zip && cp -af tmpDir/GShade-C-Shaders-main/gshade-shaders .
-    unzip -qquo iMMERSE-main.zip -d tmpDir && rm iMMERSE-main.zip && cp -af tmpDir/iMMERSE-main/Shaders gshade-shaders && cp -af tmpDir/iMMERSE-main/Textures gshade-shaders
-    unzip -qquo METEOR-main.zip -d tmpDir && rm METEOR-main.zip && cp -af tmpDir/METEOR-main/Shaders gshade-shaders && cp -af tmpDir/METEOR-main/Textures gshade-shaders
-    unzip -qquo SHADERDECK-main.zip -d tmpDir && rm SHADERDECK-main.zip && cp -af tmpDir/SHADERDECK-main/Shaders gshade-shaders && cp -af tmpDir/SHADERDECK-main/textures/* gshade-shaders/Textures && rm -r tmpDir #just case-sensitivity things
+    unzip -qquo "GShade-Presets-master.zip" -d "tmpDir/gshade-presets" && rm "GShade-Presets-master.zip" && cp -af "tmpDir/gshade-presets/GShade-Presets-master/FFXIV/*" "gshade-presets"
+    unzip -qquo "GShade-C-Shaders-main.zip" -d "tmpDir" && rm "GShade-C-Shaders-main.zip" && cp -af "tmpDir/GShade-C-Shaders-main/gshade-shaders" "."
+    unzip -qquo "iMMERSE-main.zip" -d "tmpDir" && rm "iMMERSE-main.zip" && cp -af "tmpDir/iMMERSE-main/Shaders" "gshade-shaders" && cp -af "tmpDir/iMMERSE-main/Textures" "gshade-shaders"
+    unzip -qquo "METEOR-main.zip" -d "tmpDir" && rm "METEOR-main.zip" && cp -af "tmpDir/METEOR-main/Shaders" "gshade-shaders" && cp -af "tmpDir/METEOR-main/Textures" "gshade-shaders"
+    unzip -qquo "SHADERDECK-main.zip" -d "tmpDir" && rm "SHADERDECK-main.zip" && cp -af "tmpDir/SHADERDECK-main/Shaders" "gshade-shaders" && cp -af "tmpDir/SHADERDECK-main/textures/*" "gshade-shaders/Textures" && rm -r "tmpDir" #just case-sensitivity things
   fi
   updateInstalls presets
   popd > /dev/null || exit
@@ -1125,7 +1125,7 @@ update() {
         done < "$dbFile"
       fi
     fi
-    rm -rf "$GShadeHome/gshade-shaders"
+    rm -rf "gshade-shaders"
     presetAndShaderUpdate
     if [[ -f "$GShadeHome/GShade.ini" ]]; then
       printf "Saving GShade.ini settings..."
